ca56dd9e67f9655b87efe9a247f1b824f4f249a3
ripper <wangyifani@foxmail.com>
2024-12-03 16:59:05 +0800

    添加异常响应的日志和报警通知的注释。

--- packages/global/common/error/utils.ts
+++ packages/global/common/error/utils.ts
@@ -6,5 +6,6 @@ export const getErrText = (err: any, def = ''): any => {
       ? err
       : err?.response?.data?.message || err?.response?.message || err?.message || def;
   msg && console.log('error =>', msg);
+  // todo: 添加一个异常响应的日志和报警通知
   return replaceSensitiveText(msg);
 };
