30e4adce6ee328e9f55cb2743b84d8f2d0308061
ripper <wangyifani@foxmail.com>
2025-03-08 19:17:03 +0800

    文件夹增加权限

--- projects/app/src/components/common/folder/SlideCard.tsx
+++ projects/app/src/components/common/folder/SlideCard.tsx
@@ -123,87 +123,83 @@ const FolderSlideCard = ({
         </>
       )}
 
-      {feConfigs?.isPlus && (
-        <>
-          <MyDivider my={6} />
+      <MyDivider my={6} />
 
-          <Box>
-            <FormLabel>{t('common:support.permission.Permission')}</FormLabel>
+      <Box>
+        <FormLabel>{t('common:support.permission.Permission')}</FormLabel>
 
-            {!isInheritPermission && (
-              <Box mt={2}>
-                <ResumeInherit onResume={() => resumeInheritPermission?.().then(refetchResource)} />
-              </Box>
-            )}
-
-            {managePer.permission.hasManagePer && (
-              <Box mt={5}>
-                <Box fontSize={'sm'} color={'myGray.500'}>
-                  {t('common:permission.Default permission')}
-                </Box>
-                <DefaultPermissionList
-                  mt="1"
-                  per={defaultPer.value}
-                  defaultPer={defaultPer.defaultValue}
-                  isInheritPermission={isInheritPermission}
-                  onChange={(v) => defaultPer.onChange(v)}
-                  hasParent={hasParent}
-                />
-              </Box>
-            )}
-            <Box mt={6}>
-              <CollaboratorContextProvider
-                {...managePer}
-                refreshDeps={refreshDeps}
-                refetchResource={refetchResource}
-                isInheritPermission={isInheritPermission}
-                hasParent={hasParent}
-              >
-                {({ MemberListCard, onOpenManageModal, onOpenAddMember }) => {
-                  return (
-                    <>
-                      <Flex alignItems="center" justifyContent="space-between">
-                        <Box fontSize={'sm'} color={'myGray.500'}>
-                          {t('common:permission.Collaborator')}
-                        </Box>
-                        {managePer.permission.hasManagePer && (
-                          <HStack spacing={3}>
-                            <MyTooltip label={t('common:permission.Manage')}>
-                              <MyIcon
-                                w="1rem"
-                                name="common/settingLight"
-                                cursor={'pointer'}
-                                _hover={{ color: 'primary.600' }}
-                                onClick={onOpenManageModal}
-                              />
-                            </MyTooltip>
-                            <MyTooltip label={t('common:common.Add')}>
-                              <MyIcon
-                                w="1rem"
-                                name="support/permission/collaborator"
-                                cursor={'pointer'}
-                                _hover={{ color: 'primary.600' }}
-                                onClick={onOpenAddMember}
-                              />
-                            </MyTooltip>
-                          </HStack>
-                        )}
-                      </Flex>
-                      <MemberListCard
-                        mt={2}
-                        tagStyle={{
-                          type: 'borderSolid',
-                          colorSchema: 'gray'
-                        }}
-                      />
-                    </>
-                  );
-                }}
-              </CollaboratorContextProvider>
-            </Box>
+        {!isInheritPermission && (
+          <Box mt={2}>
+            <ResumeInherit onResume={() => resumeInheritPermission?.().then(refetchResource)} />
           </Box>
-        </>
-      )}
+        )}
+
+        {managePer.permission.hasManagePer && (
+          <Box mt={5}>
+            <Box fontSize={'sm'} color={'myGray.500'}>
+              {t('common:permission.Default permission')}
+            </Box>
+            <DefaultPermissionList
+              mt="1"
+              per={defaultPer.value}
+              defaultPer={defaultPer.defaultValue}
+              isInheritPermission={isInheritPermission}
+              onChange={(v) => defaultPer.onChange(v)}
+              hasParent={hasParent}
+            />
+          </Box>
+        )}
+        <Box mt={6}>
+          <CollaboratorContextProvider
+            {...managePer}
+            refreshDeps={refreshDeps}
+            refetchResource={refetchResource}
+            isInheritPermission={isInheritPermission}
+            hasParent={hasParent}
+          >
+            {({ MemberListCard, onOpenManageModal, onOpenAddMember }) => {
+              return (
+                <>
+                  <Flex alignItems="center" justifyContent="space-between">
+                    <Box fontSize={'sm'} color={'myGray.500'}>
+                      {t('common:permission.Collaborator')}
+                    </Box>
+                    {managePer.permission.hasManagePer && (
+                      <HStack spacing={3}>
+                        <MyTooltip label={t('common:permission.Manage')}>
+                          <MyIcon
+                            w="1rem"
+                            name="common/settingLight"
+                            cursor={'pointer'}
+                            _hover={{ color: 'primary.600' }}
+                            onClick={onOpenManageModal}
+                          />
+                        </MyTooltip>
+                        <MyTooltip label={t('common:common.Add')}>
+                          <MyIcon
+                            w="1rem"
+                            name="support/permission/collaborator"
+                            cursor={'pointer'}
+                            _hover={{ color: 'primary.600' }}
+                            onClick={onOpenAddMember}
+                          />
+                        </MyTooltip>
+                      </HStack>
+                    )}
+                  </Flex>
+                  <MemberListCard
+                    mt={2}
+                    tagStyle={{
+                      type: 'borderSolid',
+                      colorSchema: 'gray'
+                    }}
+                  />
+                </>
+              );
+            }}
+          </CollaboratorContextProvider>
+        </Box>
+      </Box>
 
       <ConfirmModal />
     </Box>
